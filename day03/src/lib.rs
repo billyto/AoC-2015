da2d7dcc098109fabf4a40f7b831d27fc77a8696
Billy Tobon <billy.tobon@gmail.com>
2025-01-25 08:26:15 +0100

    day 3 - part 2 solved - serial

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -50,6 +50,54 @@ pub fn solve_part2(directions: &Vec<char>) -> i32 {
     let santa_zero: (i32, i32) = (santa_x, santa_y);
     visited_houses.insert(santa_zero);
 
+    for (index, c) in directions.iter().enumerate() {
+        let is_santa: bool = index % 2 == 0;
+
+        match c {
+            '^' => {
+                if is_santa {
+                    santa_y += 1
+                } else {
+                    robo_y += 1
+                }
+            }
+            '<' => {
+                if is_santa {
+                    santa_x -= 1
+                } else {
+                    robo_x -= 1
+                }
+            }
+            '>' => {
+                if is_santa {
+                    santa_x += 1
+                } else {
+                    robo_x += 1
+                }
+            }
+            'v' => {
+                if is_santa {
+                    santa_y -= 1
+                } else {
+                    robo_y -= 1
+                }
+            }
+            _ => panic!("Unknown direction panic!"),
+        };
+
+        let house: (i32, i32) = if is_santa {
+            (santa_x, santa_y)
+        } else {
+            (robo_x, robo_y)
+        };
+        // println!(
+        //     "{} going to {:?}",
+        //     if is_santa { "Santa" } else { "Robo" },
+        //     house
+        // );
+        visited_houses.insert(house);
+    }
+
     visited_houses.len() as i32
 }
 
@@ -76,14 +124,14 @@ mod tests {
     fn test_solve_part2() {
         // Test case 1: ^v
         let input1 = "^v".chars().collect::<Vec<char>>();
-        assert_eq!(solve_part1(&input1), 3);
+        assert_eq!(solve_part2(&input1), 3);
 
         // Test case 2: ^>v<
         let input2 = "^>v<".chars().collect::<Vec<char>>();
-        assert_eq!(solve_part1(&input2), 3);
+        assert_eq!(solve_part2(&input2), 3);
 
         // Test case 3: ^v^v^v^v^v
         let input3 = "^v^v^v^v^v".chars().collect::<Vec<char>>();
-        assert_eq!(solve_part1(&input3), 11);
+        assert_eq!(solve_part2(&input3), 11);
     }
 }
